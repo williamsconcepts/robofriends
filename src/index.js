c325979614b49f73fc7c0419d404e5ec01ad294c
williams <chidiekwebelam@.gmail.com>
2020-03-20 20:26:26 -0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots, requestRobots} from './reducers';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = 
+    createStore(rootReducer, applyMiddleware(thunkMiddleware))
+
+ReactDOM.render(
+
+                <Provider store={store}>
+                  <App />
+                
+                </Provider>, document.getElementById('root'));
 registerServiceWorker();
